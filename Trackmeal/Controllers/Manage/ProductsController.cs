51e947a6a3a3bccf72b6d0624f82383b39d6ce69
bkisly <b.kisly@gmail.com>
2022-09-09 02:30:57 +0200

    Scaffolded a controller for managing products

---
+++ Trackmeal/Controllers/Manage/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trackmeal.Controllers.Manage
+{
+    [Route("Manage/[controller]/[action]")]
+    public class ProductsController : Controller
+    {
+        [Route("~/Manage/[controller]")]
+        public string Index()
+        {
+            return "Here you can manage products";
+        }
+
+        public string New()
+        {
+            return "Adding new product";
+        }
+
+        [HttpGet("{id?}")]
+        public string Edit(int id)
+        {
+            return $"Editing product with id {id}";
+        }
+    }
+}

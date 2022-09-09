2bdf95f5c27cfc5b33cedc8d9a682f5041a2472d
bkisly <b.kisly@gmail.com>
2022-09-09 02:38:09 +0200

    Added product model

--- Trackmeal/Data/ApplicationDbContext.cs
+++ Trackmeal/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Trackmeal.Models;
 
 namespace Trackmeal.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; } = null!;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Trackmeal/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trackmeal.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [StringLength(100)] public string Name { get; set; } = null!;
+        [StringLength(1000)] public string? Description { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}

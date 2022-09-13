ce2f19fe12e26bb8a9fd28e90eafaac5fb3787de
bkisly <b.kisly@gmail.com>
2022-09-13 02:06:25 +0200

    Scaffolded a controller for an order

---
+++ Trackmeal/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trackmeal.Models;
+
+namespace Trackmeal.Controllers
+{
+    public class OrderController : Controller
+    {
+        // Gets the list of all products with the possibility to add them
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Creates a new CartEntry if the product doesn't exist, otherwise increases its amount
+        [HttpPost]
+        public IActionResult AddProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Decreases the amount of product in a CartEntry, and removes it if the amount equals 0
+        [HttpDelete]
+        public IActionResult RemoveProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Displays order summary
+        public IActionResult Summary()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Creates a new Order object and adds it to the database
+        [HttpPost]
+        public IActionResult SubmitOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

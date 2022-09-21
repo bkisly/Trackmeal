ece911232e2962867ace2c7ec9b0d9dc9a3f64ec
bkisly <b.kisly@gmail.com>
2022-09-22 00:02:19 +0200

    Created index view for order management

---
+++ Trackmeal/Areas/Manage/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trackmeal.Models;
+using Trackmeal.Services;
+
+namespace Trackmeal.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class OrdersController : Controller
+    {
+        private readonly IModifiableDataService<Order> _orderService;
+
+        public OrdersController(IModifiableDataService<Order> orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _orderService.GetItemsAsync());
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

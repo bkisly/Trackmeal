4c913821aebb2cfe2832505853121e75a34ca3e9
bkisly <b.kisly@gmail.com>
2022-12-20 13:17:58 +0100

    Added description data to order status

---
+++ Trackmeal/Data/Migrations/20221220115402_AddDescriptionToOrderStatus.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trackmeal.Data.Migrations
+{
+    public partial class AddDescriptionToOrderStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "OrderStatus",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.Sql(
+                "UPDATE OrderStatus SET Description = 'Your order has been send to the restaurant. It is awaiting for the confirmation.' " +
+                "WHERE Id = 1");
+            migrationBuilder.Sql(
+                "UPDATE OrderStatus SET Description = 'The order was confirmed by the restaurant and now is being prepared. You will get notified when it is ready to collect!' " +
+                "WHERE Id = 2");
+            migrationBuilder.Sql(
+                "UPDATE OrderStatus SET Description = 'Your order is ready. Good food is wating to be collected by you! You can approach to the collect zone and get your order.' " +
+                "WHERE Id = 3");
+            migrationBuilder.Sql(
+                "UPDATE OrderStatus SET Description = 'The order has been collected, the process is finished. Enjoy your meal!' " +
+                "WHERE Id = 4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "OrderStatus");
+        }
+    }
+}
